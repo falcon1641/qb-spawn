6a607166feabd66d97deb7e8cda6abb2a7cf1113
falcon1641 <94806141+falcon1641@users.noreply.github.com>
2022-01-17 15:27:57 -0500

    add spawn locations

--- config.lua
+++ config.lua
@@ -1,14 +1,8 @@
 QB = {}
 
 QB.Spawns = {
-    ["legion"] = {
-        coords = vector4(195.17, -933.77, 29.7, 144.5),
-        location = "legion",
-        label = "Legion Square",
-    },
-
     ["policedp"] = {
-        coords = vector4(428.23, -984.28, 29.76, 3.5),
+        coords = vector4(411.58, -1004.08, 29.4, 97.01),
         location = "policedp",
         label = "Police Department",
     },
@@ -19,9 +13,29 @@ QB.Spawns = {
         label = "Paleto Bay",
     },
 
-    ["motel"] = {
-        coords = vector4(327.56, -205.08, 53.08, 163.5),
-        location = "motel",
-        label = "Motels",
+    ["apartment5"] = {
+        coords = vector4(-255.91, -984.15, 31.22, 3.76),
+        location = "apartment5",
+        label = "Alta Apartments",
+    },
+    ["apartment1"] = {
+        coords = vector4(1618.3, 3569.38, 35.3, 294.41),
+        location = "apartment1",
+        label = "Joshua Road Apartments",
+    },
+    ["apartment2"] = {
+        coords = vector4(-1285.48, -432.38, 34.78, 288.18),
+        location = "apartment2",
+        label = "Morningwood Blvd",
+    },
+    ["apartment3"] = {
+        coords = vector4(265.14, -634.62, 42.02, 179.55),
+        location = "apartment3",
+        label = "Integrity Way",
+    },
+    ["apartment4"] = {
+        coords = vector4(-623.14, 31.75, 43.53, 281.1),
+        location = "apartment4",
+        label = "Tinsel Towers",
     },
 }
